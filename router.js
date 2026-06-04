cb673711a34304c28de7d1cab94b663d51d2f8df
Imad <imadudxn@gmail.com>
2026-06-04 17:24:02 +0100

    fix router

--- router.js
+++ router.js
@@ -2,17 +2,20 @@ const { getClientByAssistantId } = require('./db')
 const { runTool } = require('./tools')
 
 async function routeToolCall(body) {
-  const { message } = body
-  
-  const assistantId = message?.call?.assistantId
-  const toolName = message?.toolCalls?.[0]?.function?.name
-  const parameters = message?.toolCalls?.[0]?.function?.arguments
+  console.log('Full request body:', JSON.stringify(body, null, 2))
 
-  console.log('Incoming tool call:', { assistantId, toolName, parameters })
+  const assistantId = body?.call?.assistantId || body?.assistantId
+  const toolName = body?.toolCalls?.[0]?.function?.name 
+    || body?.name 
+    || body?.function?.name
+  const parameters = body?.toolCalls?.[0]?.function?.arguments 
+    || body?.parameters 
+    || body?.arguments 
+    || {}
+
+  console.log('Parsed:', { assistantId, toolName, parameters })
 
   const client = await getClientByAssistantId(assistantId)
-  if (!client) throw new Error('Unknown assistant: ' + assistantId)
-
   return runTool(toolName, parameters, client)
 }
 
