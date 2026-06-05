765b181a8edd248497a44b10570c6ecfa0da36ca
Imad <imadudxn@gmail.com>
2026-06-05 09:43:07 +0100

    fix schema

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
-const { createClient } = require('@supabase/supabase-js')
-
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_ANON_KEY,
+  {
+    db: { schema: 'public' }
+  }
 )
 
 async function getClientByAssistantId(assistantId) {
