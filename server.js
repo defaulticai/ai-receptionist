9121307147ab84c000b7a05ca76875f12f2b90fe
defaulticai <defaulticai@gmail.com>
2026-06-15 15:36:08 +0100

    Set body size limits to 50mb for JSON and URL-encoded data
    Increase the JSON and URL-encoded body size limits for incoming requests.

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const { google } = require('googleapis')
 require('dotenv').config()
 
 const app = express()
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.post('/tool-call', async (req, res) => {
   console.log('=== INCOMING REQUEST ===')
